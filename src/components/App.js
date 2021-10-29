b96b0cee99dae5254ec446182dc7f9b22e48892d
Guilherme Mesquita Rocha <guilhermemesquitarch@gmail.com>
2021-10-29 09:34:59 -0300

    functions components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div>
+            <h4>Hello world ✔</h4>
+            <h4>Developed by {props.nome}</h4>
+        </div>
+    )
+}
+
