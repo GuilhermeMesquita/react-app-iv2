4cbe3315ab8ff1785ec86b9c6c1a48658aa78727
Guilherme Mesquita Rocha <guilhermemesquitarch@gmail.com>
2021-11-01 11:45:15 -0300

    tasklist activity

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (props) => {
-    return (
-        <div>
-            <h4>Hello world ✔</h4>
-            <h4>Developed by {props.nome}</h4>
-        </div>
-    )
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            itemsQuantity: 1,
+            newItemName: "",
+            items: [
+                {
+                    id: "1",
+                    taskName: "Acordar",
+                    status: "false"
+                }
+            ]
+        }
+    }
+
+    renderItems() {
+        let actualStates = this.state.items;
+        return actualStates.map((index) => {
+            return (
+                <tr>
+                    <td>{index.id}</td>
+                    <td>{index.taskName}</td>
+                    <td>{index.status}</td>
+                </tr>
+            )
+        });
+    }
+
+    changeName(event) {
+        console.log(event.target.value);
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Nome</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderItems()}
+                    </tbody>
+                </table>
+                <div>
+                    <input value={this.state.newItemName} onChange={(event) => { this.changeName(event) }}></input>
+                </div>
+            </div >
+        )
+    }
 }
+
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
-import App2 from './components/App2'
 
 ReactDOM.render(
     <div>
-        <App nome="Guilherme Rocha" />,
-        <App2></App2>
+        <App nome="Guilherme Rocha" />
     </div>,
     document.getElementById('root')
 );
